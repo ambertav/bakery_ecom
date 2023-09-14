9482ca1fa1beac6128f0842526b76fdb474b2ae1
Amber Taveras <taveras.amber@gmail.com>
2023-09-14 01:41:30 -0400

    creates user model, migrates to create table

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from .app import db
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, String, Text, Numeric
+from sqlalchemy import Column, Integer, String, Text, Numeric, TIMESTAMP
+from enum import Enum
 
 
 # Product
@@ -31,3 +32,31 @@ class Product (db.Model) :
             'price': self.price,
             'stock': self.stock
         }
+
+
+# enum for user role
+class Role (Enum) :
+    CLIENT = 1
+    ADMIN = 2
+
+# User
+class User (db.Model) :
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(30), nullable = False)
+    email = db.Column(db.String(320), unique = True, nullable = False)
+    password = db.Column(db.String(100), nullable = False)
+    billing_address = db.Column(db.Text(), nullable = False)
+    shipping_address = db.Column(db.Text(), nullable = True) # nullable for shipping same as billing option
+    role = db.Column(db.Enum(Role), default = Role.CLIENT, nullable = False)
+    created_at = db.Column(db.TIMESTAMP(), nullable = False)
+
+    def __init__ (self, name, email, password, billing_address, shipping_address, role, created_at) :
+        self.name = name
+        self.email = email
+        self.password = password
+        self.billing_address = billing_address
+        self.shipping_address = shipping_address
+        self.role = role
+        self.created_at = created_at
