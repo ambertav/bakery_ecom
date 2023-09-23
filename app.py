5f08df664ef648fcb4f5141edbf0fe237ac46a33
Amber Taveras <taveras.amber@gmail.com>
2023-09-22 23:53:39 -0400

    adds firebase

--- app.py
+++ app.py
@@ -3,12 +3,28 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy import text
 from dotenv import load_dotenv
+import pyrebase
 import os
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+
+firebase_config = {
+    'apiKey': os.getenv('FIREBASE_API_KEY'),
+    'authDomain': "bakery-434c0.firebaseapp.com",
+    'projectId': "bakery-434c0",
+    'storageBucket': "bakery-434c0.appspot.com",
+    'messagingSenderId': "289170495122",
+    'appId': "1:289170495122:web:f0b390461c25223041784b",
+    'measurementId': "G-B3JJH6BLBW"
+}
+
+firebase = pyrebase.initialize_app(firebase_config)
+auth = firebase.auth()
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
