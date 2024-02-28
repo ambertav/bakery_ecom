9bc9583491ac2a0043b5207989422c1061646c03
Amber Taveras <taveras.amber@gmail.com>
2024-02-28 17:49:50 -0500

    separates db from app creation

--- app.py
+++ app.py
@@ -7,49 +7,54 @@ import firebase_admin
 from firebase_admin import auth, credentials
 import stripe
 import os
+
 from .config import config
+from .database import init_db
 
-load_dotenv()
+def create_app () : 
+    load_dotenv()
 
-app = Flask(__name__)
+    app = Flask(__name__)
 
-env = os.getenv('FLASK_ENV', 'development')
-app.config.from_object(config[env])
+    env = os.getenv('FLASK_ENV', 'development')
+    app.config.from_object(config[env])
 
-stripe.api_key = app.config['STRIPE_API_KEY']
-webhook_secret = app.config['WEBHOOK_SECRET']
+    stripe.api_key = app.config['STRIPE_API_KEY']
+    webhook_secret = app.config['WEBHOOK_SECRET']
 
-cred = credentials.Certificate(app.config['FIREBASE_CREDENTIALS'])
-firebase_admin.initialize_app(cred)
+    cred = credentials.Certificate(app.config['FIREBASE_CREDENTIALS'])
+    firebase_admin.initialize_app(cred)
 
-CORS(app, supports_credentials=True, origins='http://localhost:3000')
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+    CORS(app, supports_credentials = True, origins='http://localhost:3000')
+    
+    init_db(app)
 
-from .api.blueprints.product import product_bp
-from .api.blueprints.user import user_bp
-from .api.blueprints.cart_item import cart_item_bp
-from .api.blueprints.order import order_bp
-from .api.blueprints.address import address_bp
+    from .api.blueprints.product import product_bp
+    from .api.blueprints.user import user_bp
+    from .api.blueprints.cart_item import cart_item_bp
+    from .api.blueprints.order import order_bp
+    from .api.blueprints.address import address_bp
 
-app.register_blueprint(product_bp, url_prefix='/api/product')
-app.register_blueprint(user_bp, url_prefix='/api/user')
-app.register_blueprint(cart_item_bp, url_prefix = '/api/cart')
-app.register_blueprint(order_bp, url_prefix = '/api/order')
-app.register_blueprint(address_bp, url_prefix = '/api/address')
+    app.register_blueprint(product_bp, url_prefix='/api/product')
+    app.register_blueprint(user_bp, url_prefix='/api/user')
+    app.register_blueprint(cart_item_bp, url_prefix = '/api/cart')
+    app.register_blueprint(order_bp, url_prefix = '/api/order')
+    app.register_blueprint(address_bp, url_prefix = '/api/address')
 
-@app.after_request
-def after_request(response) :
-    response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
-    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
-    response.headers['Access-Control-Allow-Credentials'] = 'true'
-    return response
+    @app.after_request
+    def after_request(response) :
+        response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+        response.headers['Access-Control-Allow-Credentials'] = 'true'
+        return response
 
-@app.route('/')
-def home () :
-    return 'Hello World!'
-
+    @app.route('/')
+    def home () :
+        return 'Hello World!'
+    
+    return app
 
 if __name__ == '__main__' :
-    app.run(debug=True)
+    app = create_app()
+    app.run(debug = True)
