3d154f32b77c7e9859f1e4a42678d68a21ce063d
Amber Taveras <taveras.amber@gmail.com>
2024-04-24 18:35:45 -0400

    separates client user and admin auth functions

--- api/utils/auth.py
+++ api/utils/auth.py
@@ -1,6 +1,6 @@
 from firebase_admin import auth
 
-from ..models.models import User
+from ..models.models import User, Admin
 
 def auth_user (request) :
     if 'Authorization' not in request.headers :
@@ -18,6 +18,27 @@ def auth_user (request) :
             return None
         else :
             return user
+        
+    except Exception as error :
+        return error
+    
+        
+def auth_admin (request) :
+    if 'Authorization' not in request.headers :
+        return None
+    
+    try :
+        # decode to retrieve uid
+        token = request.headers['Authorization'].replace('Bearer ', '')
+        decoded_token = auth.verify_id_token(token)
+        uid = decoded_token['uid']
+
+        admin = Admin.query.filter_by(firebase_uid = uid).first()
+
+        if not admin :
+            return None
+        else :
+            return admin
         
     except Exception as error :
         return error
