43c59acfc93afe1db10eec6287ffa6a3b407fefa
Amber Taveras <taveras.amber@gmail.com>
2024-05-01 23:44:11 -0400

    adds admin blueprint ctrl

--- app.py
+++ app.py
@@ -19,24 +19,26 @@ def create_app () :
     env = os.getenv('FLASK_ENV', 'development')
     app.config.from_object(config[env])
 
-    stripe.api_key = app.config['STRIPE_API_KEY']
-    webhook_secret = app.config['WEBHOOK_SECRET']
+    stripe.api_key = os.getenv('STRIPE_SECRET_KEY')
+    webhook_secret = os.getenv('WEBHOOK_SECRET')
 
     cred = credentials.Certificate(app.config['FIREBASE_CREDENTIALS'])
     firebase_admin.initialize_app(cred)
 
-    CORS(app, supports_credentials = True, origins='http://localhost:3000')
+    CORS(app, supports_credentials = True, origins = '*')
     
     init_db(app)
 
     from .api.blueprints.product import product_bp
     from .api.blueprints.user import user_bp
+    from .api.blueprints.admin import admin_bp
     from .api.blueprints.cart_item import cart_item_bp
     from .api.blueprints.order import order_bp
     from .api.blueprints.address import address_bp
 
     app.register_blueprint(product_bp, url_prefix='/api/product')
     app.register_blueprint(user_bp, url_prefix='/api/user')
+    app.register_blueprint(admin_bp, url_prefix='/api/admin')
     app.register_blueprint(cart_item_bp, url_prefix = '/api/cart')
     app.register_blueprint(order_bp, url_prefix = '/api/order')
     app.register_blueprint(address_bp, url_prefix = '/api/address')
@@ -44,7 +46,7 @@ def create_app () :
     @app.after_request
     def after_request(response) :
         response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
-        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS, DELETE'
         response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
         response.headers['Access-Control-Allow-Credentials'] = 'true'
         return response
