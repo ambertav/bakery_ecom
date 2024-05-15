965cca7808925c8cd9c6461296b56109bc24426c
Amber Taveras <taveras.amber@gmail.com>
2024-05-15 13:05:32 -0400

    removes admin related tests from user tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,8 +1,6 @@
-import pytest
 from unittest.mock import patch
 
-from ..database import db
-from ..api.models.models import User, Role
+from ..api.models.models import User
 
 
 # mock firebase, user sign up
@@ -41,13 +39,8 @@ def test_login (flask_app) :
     assert response.status_code == 200
     assert response.json['message'] == 'User logged in successfully'
 
-@pytest.mark.parametrize('is_admin', [True, False])
-def test_get_user_info (flask_app, create_admin_user, create_client_user, is_admin) :
-    # initialize user based on if is_admin
-    if is_admin :
-        user, test_uid = create_admin_user
-    else :
-        user, test_uid = create_client_user
+def test_get_user_info (flask_app, create_client_user ) :
+    user, test_uid = create_client_user
 
     with patch('firebase_admin.auth.verify_id_token') as mock_verify_id_token :
         mock_verify_id_token.return_value = { 'uid': test_uid }
@@ -58,5 +51,5 @@ def test_get_user_info (flask_app, create_admin_user, create_client_user, is_adm
 
     # asserting that name and admin status is correctly sent
     assert response.status_code == 200
-    assert response.json['name'] == user.name
-    assert response.json['isAdmin'] == (True if user.role == Role.ADMIN else False)
+    assert response.json['name'] ==  user.name
+    assert response.json['isAdmin'] == False
