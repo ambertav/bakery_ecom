cbf063f95546aa32e15ca174cf9d9a20d54a3fd4
Amber Taveras <taveras.amber@gmail.com>
2024-09-07 03:17:01 -0400

    adds role and exp params for both access, refresh and adds docstrings

--- api/utils/token.py
+++ api/utils/token.py
@@ -4,19 +4,42 @@ from datetime import datetime, timezone, timedelta
 
 secret_key = os.getenv('SECRET_KEY')
 
-def generate_jwt (user_id) :
-    expiration = datetime.now(timezone.utc) + timedelta(days = 7)
+def generate_jwt (user_id, role, expiration) :
+    '''
+    Generates a jwt token for both access and refresh tokens.
+
+    Args :
+        user_id (str) : user or admin id.
+        role (str) : either user or admin
+        expiration (int) : token expiration in minutes from the current time
+
+    Returns :
+        str : JWT token with the user ID as the subject (sub) and the expiration time (exp) set.
+    '''
+
     payload = {
         'sub': user_id,
-        'exp': expiration
+        'role': role,
+        'exp': datetime.now(timezone.utc) + timedelta(minutes = expiration)
     }
 
     return jwt.encode(payload, secret_key, algorithm = 'HS256')
 
 def decode_jwt (token) :
+    '''
+    Decodes a JWT token to extract the payload, including user ID (sub) and expiration time (exp).
+
+    Args :
+        token (str) : JWT token to decode.
+
+    Returns :
+        dict : Decoded JWT payload containing 'sub' (user or admin ID), 'role' (either user or admin), and 'exp' (expiration).
+    
+    Raises :
+        Exception : If token decoding fails or the token is invalid.
+    '''
     try :
-        decoded_payload = jwt.decode(token, secret_key, algorithms=['HS256'])
-        return decoded_payload.get('sub')
+        return jwt.decode(token, secret_key, algorithms=['HS256'])
     
     except Exception as error :
         raise error
