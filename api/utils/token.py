3d65570927f0d0dd3ae1288deb08457b56aa939e
Amber Taveras <taveras.amber@gmail.com>
2024-09-09 00:40:41 -0400

    adds method to calc time in seconds until jwt expires from current

--- api/utils/token.py
+++ api/utils/token.py
@@ -44,3 +44,28 @@ def decode_jwt (token) :
     except Exception as error :
         raise error
     
+def get_time_until_jwt_expire (token) :
+    '''
+    Extracts expiration time from JWT token payload and calculates the seconds from now to that expiration time.
+
+    Args :
+        token (str) : JWT token to decode.
+
+    Returns :
+        float : number of seconds from current time until the JWT token expires.
+    
+    Raises :
+        Exception : If token decoding fails or the token is invalid.
+
+    '''
+    try :
+        expiration = jwt.decode(token, secret_key, algorithms=['HS256']).get('exp')
+        expiration_time = datetime.fromtimestamp(expiration, tz = timezone.utc)
+        current_time = datetime.now(timezone.utc)
+        
+        return (expiration_time - current_time).total_seconds()
+    
+    except Exception as error :
+        raise error
+
+    
