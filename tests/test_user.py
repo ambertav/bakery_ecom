f12b97477dcc0ff94e3e6ad28b0f9a7a9ac7d73a
Amber Taveras <taveras.amber@gmail.com>
2024-09-21 02:09:20 -0400

    updates user tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,51 +1,71 @@
 from unittest.mock import patch
 
-from ..api.models.models import User
+from ..api.models import User
 
 
-# mock firebase, user sign up
 def test_signup (flask_app) :
-    with patch('firebase_admin.auth.verify_id_token') as mock_verify_id_token :
-        mock_verify_id_token.return_value = { 'uid': 'mock_uid' }
-        input_data = { 'name': 'Test' }
+    with patch('backend.api.utils.token.generate_jwt') as mock_generate_jwt, \
+        patch('backend.api.blueprints.user.process_shopping_cart') as mock_process_cart :
 
-        response = flask_app.post('/api/user/signup',
-            headers = { 'Authorization': f'Bearer mock_uid' },
-            json = input_data,
-        )
+        mock_generate_jwt.side_effect = ['access_token_mock', 'refresh_token_mock']
+        mock_process_cart.return_value = None
 
-    assert response.status_code == 201
-    assert response.json['message'] == 'User registered successfully'
+        response = flask_app.post('/api/user/signup', 
+            json = { 
+                'name': 'Test',
+                'email': 'email@gmail.com',
+                'password': 'password',
+                'confirm_password': 'password',
+                'localStorageCart': ['item1', 'item2']
+            })
 
-    new_user = User.query.filter_by(name = input_data['name']).first()
+        assert response.status_code == 201
+        assert all(
+            any(
+                token in cookie for cookie in response.headers.getlist('Set-Cookie')
+            ) for token in ('access_token=', 'refresh_token='
+        ))
+
+    new_user = User.query.filter_by(name = 'Test').first()
     assert new_user is not None
-    assert new_user.firebase_uid == 'mock_uid'
+    assert isinstance(new_user.password, bytes)
 
 # mock firebase, user login
-def test_login (flask_app, mock_firebase) :
+def test_login (flask_app) :
     created_user = User.query.filter_by(name = 'Test').first()
 
-    with mock_firebase(created_user.firebase_uid) :
+    with patch('backend.api.utils.token.generate_jwt') as mock_generate_jwt :
+        mock_generate_jwt.side_effect = ['access_token_mock', 'refresh_token_mock']
         response = flask_app.post('/api/user/login',
-            headers = { 'Authorization': f'Bearer {created_user.firebase_uid}' },
             json = {
-                'name': '',
+                'email': 'email@gmail.com',
+                'password': 'password',
                 'localStorageCart': None
             },
         )
 
+        assert response.status_code == 200
+        assert all(
+            any(
+                token in cookie for cookie in response.headers.getlist('Set-Cookie')
+            ) for token in ('access_token=', 'refresh_token='
+        ))
+
+def test_get_user_info (flask_app) :
+    user = User.query.filter_by(name = 'Test').first()
+
+    with patch('flask.request.cookies.get') as mock_get_cookie, \
+        patch('backend.api.utils.token.decode_jwt') as mock_decode_jwt :
+
+        mock_get_cookie.return_value = 'valid_access_token'
+        mock_decode_jwt.return_value = {
+            'name': user.name,
+            'isAdmin': False,
+            'sub': user.id
+        }
+
+        response = flask_app.get('/api/user/info')
+
     assert response.status_code == 200
-    assert response.json['message'] == 'User logged in successfully'
-
-def test_get_user_info (flask_app, create_client_user, mock_firebase) :
-    user, test_uid = create_client_user
-
-    with mock_firebase(test_uid) :
-        response = flask_app.get('/api/user/info',
-            headers = { 'Authorization': f'Bearer {test_uid}' }
-        )
-
-    # asserting that name and admin status is correctly sent
-    assert response.status_code == 200
-    assert response.json['name'] ==  user.name
-    assert response.json['isAdmin'] == False
+    assert response.json['user']['name'] ==  user.name
+    assert response.json['user']['isAdmin'] == False
