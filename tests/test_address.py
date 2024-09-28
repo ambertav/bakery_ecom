8fe0225424d9b78680d4256f4532a5c57dd357e1
Amber Taveras <taveras.amber@gmail.com>
2024-09-28 01:33:52 -0400

    updates address tests

--- tests/test_address.py
+++ tests/test_address.py
@@ -1,63 +1,66 @@
 import pytest
 import unittest
+from unittest.mock import patch
 import datetime
 
 from sqlalchemy import desc
 from sqlalchemy.exc import IntegrityError, DataError
 
+
 from ..database import db
-from ..api.models.models import Address, Order, Order_Status, Deliver_Method, Pay_Status
+from ..api.models import Address, Order
+from ..api.models.order import Order_Status, Deliver_Method, Pay_Status
 
 
 @pytest.fixture(scope = 'module')
-def seed_addresses (flask_app, create_client_user) :
-    user, test_uid = create_client_user
+def seed_addresses (create_client_user) :
+    user = create_client_user
 
-    address_data = [
-        {
-            'first_name': 'Emily',
-            'last_name': 'Smith',
-            'street': '123 Main St',
-            'city': 'Anytown',
-            'state': 'NY',
-            'zip': '10001'
-        },
-        {
-            'first_name': 'Sophia', 
-            'last_name': 'Williams',
-            'street': '456 Elm St',
-            'city': 'Sometown',
-            'state': 'CA',
-            'zip': '90001'
-        },
-        {
-            'first_name': 'Alice',
-            'last_name': 'Johnson',
-            'street': '789 Oak St',
-            'city': 'Othertown',
-            'state': 'TX',
-            'zip': '75001'
-        },
+    addresses = [
+        Address(
+            first_name = 'Emily',
+            last_name = 'Smith',
+            street = '123 Main St',
+            city = 'Anytown',
+            state = 'NY',
+            zip = '10001',
+            default = False,
+            user_id = user.id,
+        ),
+        Address(
+            first_name = 'Sophia', 
+            last_name = 'Williams',
+            street = '456 Elm St',
+            city = 'Sometown',
+            state = 'CA',
+            zip = '90001',
+            default = False,
+            user_id = user.id,
+        ),
+        Address(
+            first_name = 'Alice',
+            last_name = 'Johnson',
+            street = '789 Oak St',
+            city = 'Othertown',
+            state = 'TX',
+            zip = '75001',
+            default = False,
+            user_id = user.id,
+        ),
     ]
-
-    for address in address_data :
-        address['default'] = False
-        address['user_id'] = user.id
-
-    session = db.session()
 
     try:
         # insert address in bulk
-        session.bulk_insert_mappings(Address, address_data)
-        session.commit()
+        db.session.add_all(addresses)
+        db.session.commit()
 
         # returns list of all addresses for use throughout module tests
         # orders default address first in list
         yield Address.query.filter_by(user_id = user.id).order_by(desc(Address.default)).all()
 
     finally:
-        session.rollback()
-        session.close()
+        db.session.rollback()
+        db.session.close()
 
 
 # testing zip code validation
@@ -69,7 +72,7 @@ def seed_addresses (flask_app, create_client_user) :
 ])
 def test_address_validation (flask_app, create_client_user, first_name, last_name, street, city, state, zip, default, valid) :
     # create user
-    user, test_uid = create_client_user
+    user = create_client_user
 
     if valid :       
         # base case with valid data, should create address
@@ -85,39 +88,43 @@ def test_address_validation (flask_app, create_client_user, first_name, last_nam
         db.session.rollback() # rollback failed transaction in database 
         assert error.type in (IntegrityError, DataError) # assert IntegrityError
 
-
 @pytest.mark.parametrize('requesting_default', (True, False))
-def test_get_address (flask_app, create_client_user, mock_firebase, seed_addresses, requesting_default) :
+def test_get_address (flask_app, create_client_user, user_login, seed_addresses, requesting_default) :
+    user_login
+
     # create user and get list of addresses
-    user, test_uid = create_client_user
+    user = create_client_user
     addresses = seed_addresses
 
     # if user requesting default, query param of default=true
     query_params = { 'default': 'true' } if requesting_default else {}
 
-    with mock_firebase(test_uid) :
+    with patch('backend.api.utils.token.decode_jwt') as mock_decode_jwt :
+        mock_decode_jwt.return_value = {
+            'sub': user.id,
+            'role': 'role'
+        }
+
         response = flask_app.get('/api/address/',
-            headers = { 'Authorization': f'Bearer {test_uid}' }, 
             query_string = query_params,
         )
-    
-    assert response.status_code in [200, 308]
+        assert response.status_code == 200
 
-    # if default=true query param
-    if requesting_default :
-        # query for default address, assert that its same as response 
-        default_address = Address.query.filter_by(user_id = user.id, default = True).all()
-        unittest.TestCase().assertDictEqual(default_address[0].as_dict(), response.json['addresses'])
+            # if default=true query param
+        if requesting_default :
+            # query for default address, assert that its same as response 
+            default_address = Address.query.filter_by(user_id = user.id, default = True).all()
+            unittest.TestCase().assertDictEqual(default_address[0].as_dict(), response.json['addresses'])
 
-    else :
-        # asserting that response sends all the addresses
-        assert len(response.json['addresses']) == len(addresses)
-
+        else :
+            # asserting that response sends all the addresses
+            assert len(response.json['addresses']) == len(addresses)
 
 @pytest.mark.parametrize('valid_address', [(True), (False)])
-def test_update_default (flask_app, create_client_user, mock_firebase, seed_addresses, valid_address) :
+def test_update_default (flask_app, create_client_user, user_login, seed_addresses, valid_address) :
+    user_login
     # create user and get list of addresses
-    user, test_uid = create_client_user
+    user = create_client_user
     addresses = seed_addresses
 
     previous_default = addresses[0]
@@ -131,10 +138,13 @@ def test_update_default (flask_app, create_client_user, mock_firebase, seed_addr
         # otherwise, initialize id with an invalid id
         address_id = 0
 
-    with mock_firebase(test_uid) :
-        response = flask_app.put(f'/api/address/default/{address_id}',
-            headers = { 'Authorization': f'Bearer {test_uid}' },
-        )
+    with patch('flask.request.cookies.get') as mock_get_cookie, \
+        patch('backend.api.utils.token.decode_jwt') as mock_decode_jwt :
+
+        mock_get_cookie.return_value = 'valid_access_token'
+        mock_decode_jwt.return_value = { 'sub': user.id, 'role': 'user' }
+
+        response = flask_app.put(f'/api/address/default/{address_id}')
 
     if valid_address :
         assert response.status_code == 200
@@ -157,9 +167,11 @@ def test_update_default (flask_app, create_client_user, mock_firebase, seed_addr
     (True, False, True), # valid id, not default, used for order --> 400 error, violates not null in order table
     (False, None, None) # invalid id, 404 not found
 ])
-def test_delete_address (flask_app, create_client_user, mock_firebase, seed_addresses, valid_id, is_default, used_for_order) :
+def test_delete_address (flask_app, create_client_user, user_login, seed_addresses, valid_id, is_default, used_for_order) :
+    user_login
+    
     # create user and get list of addresses
-    user, test_uid = create_client_user
+    user = create_client_user
     addresses = seed_addresses
 
     if valid_id :
@@ -176,10 +188,13 @@ def test_delete_address (flask_app, create_client_user, mock_firebase, seed_addr
     else :
         address_id = 0
 
-    with mock_firebase(test_uid) :
-        response = flask_app.delete(f'/api/address/{address_id}/delete',
-            headers = { 'Authorization': f'Bearer {test_uid}' },
-        )
+    with patch('flask.request.cookies.get') as mock_get_cookie, \
+        patch('backend.api.utils.token.decode_jwt') as mock_decode_jwt :
+
+        mock_get_cookie.return_value = 'valid_access_token'
+        mock_decode_jwt.return_value = { 'sub': user.id, 'role': 'user' }
+        
+        response = flask_app.delete(f'/api/address/{address_id}/delete')
 
     if valid_id :
         if used_for_order :
@@ -203,8 +218,6 @@ def test_delete_address (flask_app, create_client_user, mock_firebase, seed_addr
     else :
         assert response.status_code == 404
         assert response.json['error'] == 'Address not found'
-    
-
 
 
 # ---- helpers ----
@@ -231,9 +244,7 @@ def create_order_and_associate_address (address_id, user_id) :
     order = Order(
         user_id = user_id,
         total_price = 10.00,
-        date = datetime.datetime.now(),
         status = Order_Status.PENDING,
-        stripe_payment_id = None,
         delivery_method = Deliver_Method.STANDARD,
         payment_status = Pay_Status.PENDING,
         shipping_address_id = address_id
