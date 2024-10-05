ebaf77687a811ec63583054587d721fa145f0ebb
Amber Taveras <taveras.amber@gmail.com>
2024-10-05 01:35:31 -0400

    updates product tests

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,69 +1,85 @@
 import pytest
 import unittest
 import random
+import os
 
+
+from unittest.mock import patch
 from sqlalchemy.exc import IntegrityError
 
 from ..database import db
-from ..api.models.models import Product, Category
+from ..api.models import Product, Category, Role
 
 
-# model validation tests
-@pytest.mark.parametrize('name, description, category, image, price, stock, valid', [
-    ('Product 1', 'Description 1', Category.CAKE, 'https://example.com/image.jpg', 10.00, 100, True),  # valid
-    ('Product 2', 'Description 2', Category.CUPCAKE, 'https://example.com/image.jpg', -5.00, 100, False),  # invalid, violates negative price
-    ('Product 3', 'Description 3', Category.PIE, 'https://example.com/image.jpg', 10.00, -50, False),  # invalid, violates negative stock
-    ('Product 4', 'Description 4', Category.CAKE, 'image.png', 10.00, 100, False),  # invalid, violates image string format
-])
-def test_product_validation(flask_app, name, description, category, image, price, stock, valid) :
-    if valid :
-        # base case with valid data, should create product
-        create_and_add_product(name, description, category, image, price, stock)
-        added_product = Product.query.filter_by(name = name).first()
-        assert added_product is not None
-    else:
-        # testing column constraints by asserting presence of IntegrityError
+@pytest.mark.parametrize('valid_image', [True, False])
+def test_product_validation(valid_image) :
+    product = Product(
+        name = 'Product 1',
+        description = 'Description 1',
+        category = Category.CAKE
+    )
+
+    db.session.add(product)
+    db.session.commit()
+    db.session.refresh(product)
+
+    image_url = "https://s3.amazonaws.com/bucket/images/product123.jpg" if valid_image else "invalid"
+
+    if valid_image :
+        product.update_attributes({ 'image': image_url })
+        assert product.image == image_url
+    else :
         with pytest.raises(IntegrityError) as error :
-            create_and_add_product(name, description, category, image, price, stock)
+            product.update_attributes({ 'image': image_url })
+            db.session.commit()
+
         db.session.rollback() # rollback failed transaction in database 
-        assert error.type is IntegrityError # assert IntegrityError
+        assert error.type is IntegrityError
+
 
 
 # controller logic tests
-@pytest.mark.parametrize('role', ['ADMIN', 'CLIENT'])
-def test_product_creation(flask_app, create_admin_user, create_client_user, mock_firebase, role) :
-    # creates users with different roles
-    if role == 'ADMIN' :
-        admin, test_uid = create_admin_user 
-    else :
-        user, test_uid = create_client_user
+@pytest.mark.parametrize('role', [Role.SUPER, Role.MANAGER, Role.GENERAL])
+def test_product_creation(flask_app, create_admin_user, admin_login, role) :
+    admin_login
+    admin = create_admin_user
 
-    # req to create product
-    with mock_firebase(test_uid) :
-        response = flask_app.post('/api/product/create', 
-            headers = { 'Authorization': f'Bearer {test_uid}' }, 
-            json = {
-                'name': 'Admin Test Product',
-                'description': 'Test Description',
-                'category': 'COOKIE',
-                'image': 'https://example.com/image.jpg',
-                'price': 10.0,
-                'stock': 0
-            },
+    admin.role = role
+    db.session.commit()
+
+    image_path = os.path.join(os.path.dirname(__file__), 'pytestlogo.png')
+
+    with patch('flask.request.cookies.get') as mock_get_cookie, \
+        patch('backend.api.utils.token.decode_jwt') as mock_decode_jwt :
+
+        mock_get_cookie.return_value = 'valid_access_token'
+        mock_decode_jwt.return_value = { 'sub': admin.id, 'role': 'admin' }
+
+        with open(image_path, 'rb') as image_file:
+            response = flask_app.post('/api/product/create', 
+                content_type = 'multipart/form-data',
+                data = {
+                    'name': 'Test Product',
+                    'description': 'Test Description',
+                    'category': 'COOKIE',
+                    'price': 10.00,
+                    'image': (image_file, 'pytestlogo.png')
+                },
         )
 
-    if role == 'ADMIN' :
-        # admin user should create product
+    if role == Role.SUPER :
         assert response.status_code == 201
         assert response.json['message'] == 'Product created successfully'
-    else:
+        
+    else :
         # client user should get auth failed
-        assert response.status_code == 401
-        assert response.json['error'] == 'Authentication failed'
+        assert response.status_code == 403
+        assert response.json['error'] == 'Forbidden'
     
-    # runs for both tests to assert only admin's product was created
-    count_of_products = Product.query.filter_by(name = 'Admin Test Product').count()
-    assert count_of_products is 1
+    # runs for all tests to assert only SUPER admin's product was created
+    count_of_created_products = Product.query.filter_by(name = 'Test Product').count()
+    assert count_of_created_products is 1
+
 
 @pytest.mark.parametrize('requesting_category, searching', [
     (False, False), # no category or search params
@@ -97,47 +113,17 @@ def test_product_index (flask_app, requesting_category, searching) :
         count_query = count_query.filter(Product.name.ilike(f'%{search_param}%'))
 
     # assert that length of response products equals count of products
-    count_of_products = count_query.filter(Product.stock > 0).count()
+    count_of_products = count_query.count()
     assert len(response.json['products']) == count_of_products
 
     # asserting message if no products
-    if count_of_products is 0 :
+    if count_of_products == 0 :
         unittest.TestCase().assertListEqual([], response.json['products'])
         assert response.json['message'] == 'No products found'
-
-
-@pytest.mark.parametrize('role', ['ADMIN', 'CLIENT'])
-def test_view_of_product_index (flask_app, create_admin_user, create_client_user, mock_firebase, role) :
-    if role == 'ADMIN' :
-        admin, test_uid = create_admin_user
-    else :
-        user, test_uid = create_client_user
-
-    with mock_firebase(test_uid) :
-        response = flask_app.get('/api/product/', 
-            headers = { 'Authorization': f'Bearer {test_uid}' }, 
-        )
-    
-    assert response.status_code in [200, 308]
-
-    if role == 'ADMIN' :
-        # asserting that admin get out of stock products returned (all products)
-        count_of_products = Product.query.count()
-        assert len(response.json['products']) is count_of_products
-    
-    else :
-        # aasserting that clients only get in stock products
-        count_of_products = Product.query.filter(Product.stock > 0).count()
-        assert len(response.json['products']) is count_of_products
-
 
 def test_product_show (flask_app) :
     product = Product.query.filter_by(name = 'Product 1').first()
     assert product is not None
-
-    # convert price and stock to a string for last assertions against response product
-    product.price = str(product.price)
-    product.stock = str(product.stock)
 
     response = flask_app.get(f'/api/product/{product.id}')
 
@@ -146,37 +132,36 @@ def test_product_show (flask_app) :
 
 
 @pytest.mark.parametrize('role, valid_product', [
-    ('ADMIN', True),
-    ('ADMIN', False),
-    ('CLIENT', None),
+    (Role.SUPER, True),
+    (Role.SUPER, False),
+    ( Role.MANAGER, True),
+    ( Role.MANAGER, False),
+    (Role.GENERAL, None),
 ])
-def test_product_update (flask_app, create_admin_user, create_client_user, mock_firebase, role, valid_product) :
-    # creates users with different roles
-    if role == 'ADMIN' :
-        admin, test_uid = create_admin_user 
-    else :
-        user, test_uid = create_client_user
+def test_product_update (flask_app, create_admin_user, admin_login, role, valid_product) :
+    admin_login
+    admin = create_admin_user
 
-    if valid_product :
-        product = Product.query.first()
-        product_id = product.id
-    else :
-        product_id = 0
-    
+    admin.role = role
+    db.session.commit()
+
+    product = Product.query.first()
+    product_id = product.id if valid_product else 0
+
     updated_data = {
-        'name': 'Updated product',
+        'name': 'updated product',
         'description': 'updated description',
-        'stock': 10,
     }
 
-    # req to edit product
-    with mock_firebase(test_uid) :
-        response = flask_app.put(f'/api/product/{product_id}/update', 
-            headers = { 'Authorization': f'Bearer {test_uid}' }, 
-            json = updated_data
-        )
+    with patch('flask.request.cookies.get') as mock_get_cookie, \
+        patch('backend.api.utils.token.decode_jwt') as mock_decode_jwt :
+
+        mock_get_cookie.return_value = 'valid_access_token'
+        mock_decode_jwt.return_value = { 'sub': admin.id, 'role': 'admin' }
+
+        response = flask_app.put(f'/api/product/{product_id}/update', data = updated_data)
     
-    if role == 'ADMIN' and valid_product :
+    if role != Role.GENERAL and valid_product :
         assert response.status_code == 200
         assert response.json['message'] == 'Product updated successfully'
 
@@ -185,65 +170,59 @@ def test_product_update (flask_app, create_admin_user, create_client_user, mock_
         for field, value in updated_data.items():
             assert getattr(updated_product, field) == value
 
-    elif role == 'ADMIN' and not valid_product :
+    elif role != Role.GENERAL and not valid_product :
         assert response.status_code == 404
         assert response.json['error'] == 'Product not found'
 
     else :
-        assert response.status_code == 401
-        assert response.json['error'] == 'Authentication failed'
+        assert response.status_code == 403
+        assert response.json['error'] == 'Forbidden'
 
+@pytest.mark.parametrize('valid_products, valid_portions', [
+    (True, True),
+    (True, False),
+    (False, None)
+])
+def test_product_update_inventory (flask_app, create_admin_user, admin_login, valid_products, valid_portions) :
+    admin_login
+    admin = create_admin_user
 
-@pytest.mark.parametrize('valid_product_ids', [True, False])
-def test_product_update_inventory (flask_app, create_admin_user, mock_firebase, valid_product_ids) :
-    user, test_uid = create_admin_user
+    input = {}
+    previous_stock = []
 
-    if valid_product_ids :
-        # query for valid product ids
-        product_ids = db.session.query(Product.id).limit(2)
+    if valid_products :
+        products = Product.query.filter(Product.portions.any()).limit(2).all()
 
-        # dict comprehension to form structure of {'product.id': 'new stock value'}
-        # query returns ids as tuple, and will have to convert to strings
-        new_stock_value = str(random.randint(0, 50))
-        input = { str(id[0]): new_stock_value for id in product_ids }
+        for product in products :
+            if valid_portions : 
+                previous_stock.append(product.portions[0].stock)
+                
+                new_stock_value = str(random.randint(0, 50))
+                input[str(product.id)] = { str(product.portions[0].id): new_stock_value }
+            else :
+                input[str(product.id)] = { '0': '25' }
 
     else :
         input = {
-            '0': '50',
-            '0': '60',
+            '0': { '0': 10 },
+            '0': { '0': 20 },
         }
 
-    # req to edit product
-    with mock_firebase(test_uid) :
-        response = flask_app.put('/api/product/inventory/update', 
-            headers = { 'Authorization': f'Bearer {test_uid}' }, 
-            json = input
-        )
+    with patch('flask.request.cookies.get') as mock_get_cookie, \
+        patch('backend.api.utils.token.decode_jwt') as mock_decode_jwt :
 
-    if valid_product_ids :
+        mock_get_cookie.return_value = 'valid_access_token'
+        mock_decode_jwt.return_value = { 'sub': admin.id, 'role': 'admin' }
+        
+        response = flask_app.put('/api/product/inventory/update', json = input)
+
+    if valid_products and valid_portions :
         assert response.status_code == 200
         assert response.json['message'] == 'Inventory updated successfully'
 
         # retrieve one of the product ids to assert new stock value
-        updated_product = Product.query.get(product_ids[0][0])
-        assert updated_product.stock == int(new_stock_value)
+        updated_product = Product.query.get(products[0].id)
+        assert updated_product.portions[0].stock == previous_stock[0] + int(new_stock_value)
 
     else :
         assert response.status_code == 500
-    
-
-# ---- helpers ----
-
-def create_and_add_product(name, description, category, image, price, stock) :
-    product = Product(
-        name = name,
-        description = description,
-        category = category,
-        image = image,
-        price = price,
-        stock = stock
-    )
-    db.session.add(product)
-    db.session.commit()
-
-    return product
